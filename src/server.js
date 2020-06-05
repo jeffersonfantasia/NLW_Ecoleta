afeb50b97aa690cbd9c4faf5e2ff36e1a9c99229
Jefferson Fantasia <jefferson.fantasia@gmail.com>
2020-06-05 17:28:37 -0300

    newproject

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const server = express()
+
+
+server.use(express.static("public"))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+server.listen(3000)
+
+server.get("/", (req, res) => {
+    return res.render("index.html", { title: "Um titulo"})
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
+
+
+
+
